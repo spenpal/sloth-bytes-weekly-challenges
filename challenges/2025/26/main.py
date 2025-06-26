3dde30677027dcb1a839811aaa5571013bca1bab
spenpal <spenpal2000@gmail.com>
2025-06-26 08:53:14 -0500

    refactor: simplify spiral order matrix implementation by removing unnecessary variables and logic

--- challenges/2025/26/main.py
+++ challenges/2025/26/main.py
@@ -1,36 +1,28 @@
-DIRECTIONS = ((0, 1), (1, 0), (0, -1), (-1, 0))
-
-
-def out_of_bounds(
-    visited: set[tuple[int, int]], row_len: int, col_len: int, pos: tuple[int, int]
-) -> bool:
-    return pos in visited or not (0 <= pos[0] < row_len) or not (0 <= pos[1] < col_len)
-
-
 def spiral_order(matrix: list[list[int]]) -> list[int]:
-    if not matrix:
+    if not matrix or not matrix[0]:
         return []
-    res = []
-    visited = set()
-    row_len = len(matrix)
-    col_len = len(matrix[0])
-    num_of_elements = row_len * col_len
-    pos = (0, 0)
-    direction_index = 0
-    while len(visited) < num_of_elements:
-        row, col = pos
-        visited.add(pos)
-        res.append(matrix[row][col])
 
-        next_pos = (
-            row + DIRECTIONS[direction_index % len(DIRECTIONS)][0],
-            col + DIRECTIONS[direction_index % len(DIRECTIONS)][1],
-        )
-        if out_of_bounds(visited, row_len, col_len, next_pos):
-            direction_index += 1
-            next_pos = (
-                row + DIRECTIONS[direction_index % len(DIRECTIONS)][0],
-                col + DIRECTIONS[direction_index % len(DIRECTIONS)][1],
-            )
-        pos = next_pos
+    res = []
+    top, bottom = 0, len(matrix) - 1
+    left, right = 0, len(matrix[0]) - 1
+
+    while top <= bottom and left <= right:
+        # Traverse from left to right along the top row
+        res.extend(matrix[top][col] for col in range(left, right + 1))
+        top += 1
+
+        # Traverse from top to bottom along the right column
+        res.extend(matrix[row][right] for row in range(top, bottom + 1))
+        right -= 1
+
+        if top <= bottom:
+            # Traverse from right to left along the bottom row
+            res.extend(matrix[bottom][col] for col in range(right, left - 1, -1))
+            bottom -= 1
+
+        if left <= right:
+            # Traverse from bottom to top along the left column
+            res.extend(matrix[row][left] for row in range(bottom, top - 1, -1))
+            left += 1
+
     return res
